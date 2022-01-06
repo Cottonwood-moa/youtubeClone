328bc182b378d2e4eb264e56334a74f7d5856f71
Cottonwood <blog_geon@naver.com>
2022-01-06 17:42:59 +0900

    env 추가

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -6,26 +6,25 @@ class Youtube {
       redirect: "follow",
     };
   }
-  search(query) {
-    return fetch(
+  async search(query) {
+    const response = await fetch(
       `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&key=${this.key}&Key=${this.key}`,
       this.getRequestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => result.items);
-  }
-  mostPopular() {
-    return (
-      fetch(
-        `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&type=video&key=${this.key}&Key=${this.key}`,
-        this.getRequestOptions
-      )
-        .then((response) => response.json())
-        // object 를 return 할 때는 소괄호로 묶어야 한다.
-        .then((result) =>
-          result.items.map((item) => ({ ...item, id: item.id.videoId }))
-        )
     );
+    const result_1 = await response.json();
+    return result_1.items;
+  }
+  async mostPopular() {
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&type=video&key=${this.key}&Key=${this.key}`,
+      this.getRequestOptions
+    );
+    const result = await response.json();
+    const result_1 = await result.items.map((item) => ({
+      ...item,
+      id: item.id.videoId,
+    }));
+    return result_1;
   }
 }
 

eac406505234b60a0cc1c74dfa7f97a1323f963a
Cottonwood <blog_geon@naver.com>
2022-01-07 17:16:28 +0900

    반응형 추가

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,7 @@ import styles from "./app.module.css";
 import VideoList from "./components/video_list/video_list";
 import SearchHeader from "./components/search_header/search_header";
 import VideoDetail from "./components/video_detail/video_detail";
+import Test from "./components/test";
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
---
+++ src/components/test.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./test.module.scss";
+const test = (props) => {
+  return (
+    <div className={styles.test}>
+      <div className={styles.test2}></div>
+    </div>
+  );
+};
+
+export default test;
